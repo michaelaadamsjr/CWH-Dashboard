a0b8b1345f7d18659264c3e6586e3088d64dfda9
michaelaadamsjr <michaelaadamsjr@users.noreply.github.com>
2026-03-22 14:08:57 -0700

    Configure GitHub Actions deployment for Pages

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+    base: '/CWH-Dashboard/',
     plugins: [react()],
     build: {
         rollupOptions: {
