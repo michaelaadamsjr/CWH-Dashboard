25dced81e94295e6433d8864af79f6d9ab76a3d3
michaelaadamsjr <michaelaadamsjr@users.noreply.github.com>
2026-03-22 14:22:57 -0700

    Remove GitHub pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-    base: '/CWH-Dashboard/',
     plugins: [react()],
     build: {
         rollupOptions: {
